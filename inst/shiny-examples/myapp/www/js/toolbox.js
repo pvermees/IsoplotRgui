c20b1278196c2497ab8d2d2514e8b784506b480d
pvermees <p.vermeesch@ucl.ac.uk>
2018-12-08 19:05:44 +0000

    Found the problem with the JSON parser. Reverted to old version in anticipation of a permanent fix.

--- inst/shiny-examples/myapp/www/js/toolbox.js
+++ inst/shiny-examples/myapp/www/js/toolbox.js
@@ -43,7 +43,7 @@ function setSignificantDigits(x,n){
 }
 
 function patchJSON(n,o){
-    if ($.type(o) === 'array' || $.isEmptyObject(o)){
+    if ($.type(o) !== 'object' || $.isEmptyObject(o)){
         return n;
     }
     for (var k in o){
