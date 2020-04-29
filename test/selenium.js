3083ccd19be636ea6981a2e2a8ad691411c5eaa9
Tim Band <t.band>
2020-04-29 19:42:10 +0100

    Added a paste from tab separated data

--- test/selenium.js
+++ test/selenium.js
@@ -3,6 +3,7 @@
 const { spawn } = require('child_process');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const Mocha = require('mocha');
+const clipboardy = require("clipboardy");
 
 const mocha = new Mocha();
 
@@ -25,10 +26,21 @@ describe('IsoplotRgui', function testConcordia() {
     it('Concordia', async function testConcordia() {
         this.timeout(8000);
         await driver.get('http://localhost:50054');
-        let cell = await driver.wait(until.elementLocated(By.css('table tbody tr:first-of-type td:first-of-type')));
-        await cell.click();
+        await goToCell(driver, 1, 1);
         const box = await driver.switchTo().activeElement();
         await box.sendKeys('34.5', Key.TAB, '0.03', Key.RETURN);
+        clipboardy.writeSync('23.4\t0.02\n33.3\t0.023');
+        await goToCell(driver, 2, 1);
+        await box.sendKeys(Key.CONTROL + 'v');
         await driver.findElement(By.id('plot')).click();
     });
 });
+
+async function goToCell(driver, row, column) {
+    let cell = await driver.wait(until.elementLocated(cellInTable(row, column)));
+    cell.click();
+}
+
+function cellInTable(row, column) {
+    return By.css('table tbody tr:nth-of-type(' + row + ') td:nth-of-type(' + column + ')');
+}
