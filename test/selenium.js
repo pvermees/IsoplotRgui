1c4020353f1f2f93a061564c5644ed837c25ade9
Tim Band <t.band>
2020-05-01 14:18:22 +0100

    Clears example data, some refactoring

--- test/selenium.js
+++ test/selenium.js
@@ -24,17 +24,42 @@ describe('IsoplotRgui', function testConcordia() {
     })
 
     it('Concordia', async function testConcordia() {
-        this.timeout(8000);
+        this.timeout(10000);
         await driver.get('http://localhost:50054');
-        await goToCell(driver, 1, 1);
-        const box = await driver.switchTo().activeElement();
-        await box.sendKeys('34.5', Key.TAB, '0.03', Key.RETURN);
-        clipboardy.writeSync('23.4\t0.02\n33.3\t0.023');
-        await goToCell(driver, 2, 1);
-        await box.sendKeys(Key.CONTROL + 'v');
-        await driver.findElement(By.id('plot')).click();
+        await driver.wait(driver => tryToClearGrid(driver), 3000);
+        const u235toU238 = 137.818;
+        const testData = [
+            ['25.2', '0.03', '0.0513', '0.0001'],
+            ['25.4', '0.02', '0.0512', '0.0002'],
+            ['27.1', '0.01', '0.05135', '0.00005']
+        ];
+        await inputTestData(driver, testData);
     });
 });
+
+// Clicks 'Clear' button then reports if the grid (or at least the home cell)
+// did get cleared.
+async function tryToClearGrid(driver) {
+    clickButton(driver, 'clear');
+    const homeCell = driver.findElement(cellInTable(1, 1));
+    return await homeCell.getText() === '';
+}
+
+async function inputTestData(driver, testData) {
+    await goToCell(driver, 1, 1);
+    const box = await driver.switchTo().activeElement();
+    // write the first line in by typing it
+    await box.sendKeys(testData[0][0], Key.TAB, testData[0][1], Key.TAB, testData[0][2], Key.TAB, testData[0][3], Key.RETURN);
+    // write the other lines in by pasting them
+    clipboardy.writeSync(testData.slice(1).map(ds => ds.join('\t')).join('\n'));
+    await goToCell(driver, 2, 1);
+    const box2 = await driver.switchTo().activeElement();
+    await box2.sendKeys(Key.CONTROL + 'v');
+}
+
+async function clickButton(driver, id) {
+    await driver.findElement(By.id(id)).click();
+}
 
 async function goToCell(driver, row, column) {
     let cell = await driver.wait(until.elementLocated(cellInTable(row, column)));
