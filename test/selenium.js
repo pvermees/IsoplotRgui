8ca39c32fabcea7bc139b88c2e77185b6ef6004b
Tim Band <t.band>
2020-05-20 18:38:51 +0100

    Tests language switching

--- test/selenium.js
+++ test/selenium.js
@@ -2,16 +2,11 @@
 
 const { spawn } = require('child_process');
 const { Builder, By, Key, until } = require('selenium-webdriver');
-const Mocha = require('mocha');
+const { describe, before, after, it } = require('mocha');
 const clipboardy = require("clipboardy");
 const assert = require("assert");
 
-const mocha = new Mocha();
-
-mocha.addFile('test/selenium.js');
-mocha.run(function(failures) {process.exitCode = failures? 1 : 0});
-
-describe('IsoplotRgui', function testGui() {
+describe('IsoplotRgui', function() {
     let rProcess;
     let driver;
 
@@ -27,52 +22,92 @@ describe('IsoplotRgui', function testGui() {
         driver.quit();
     })
 
-    it('undoes mistakes', async function() {
-        this.timeout(20000);
-        await driver.get('http://localhost:50054');
-        await testUndoInTable(driver);
-    });
-
-    it('resists script injection attempts', async function() {
-        this.timeout(20000);
-        await driver.get('http://localhost:50054');
-        await goToCell(driver, 'INPUT', 1, 1);
-        const box = await driver.switchTo().activeElement();
-        const text = "<script>alert('bad!')</script>";
-        await box.sendKeys(text, Key.TAB);
-        await driver.wait(until.elementTextContains(box, text));
+    describe('table implementation', function() {
+        it('undoes mistakes', async function() {
+            this.timeout(20000);
+            await driver.get('http://localhost:50054');
+            await testUndoInTable(driver);
         });
 
-    it('calculates ages', async function() {
-        this.timeout(20000);
-        await driver.get('http://localhost:50054');
-        await driver.wait(until.elementLocated(cellInTable('INPUT', 1, 1)));
-        await driver.wait(driver => tryToClearGrid(driver), 3000);
-        const u235toU238 = 137.818;
-        const testData = [
-            ['25.2', '0.03', '0.0513', '0.0001'],
-            ['25.4', '0.02', '0.0512', '0.0002'],
-            ['27.1', '0.01', '0.05135', '0.00005']
-        ];
-        await inputTestData(driver, testData);
-        const agesChoiceId = 'ui-id-9';
-        await choosePlotDevice(driver, agesChoiceId);
-        await clickButton(driver, 'run');
-        const expectedResults = [
-            [251.1, 0.51, 250.86, 0.29, 253.3, 4.48, 250.88, 0.29],
-            [248.92, 0.88, 248.93, 0.19, 248.81, 8.99, 248.93, 0.19],
-            [235.59, 0.22, 233.591, 0.085, 255.54, 2.24, 233.619, 0.085]
-        ];
-        await chainWithIndex(expectedResults, (row, rowNumber) =>
-            chainWithIndex(row, (value, columnNumber) => {
-            return driver.findElement(
-                    cellInTable('OUTPUT', rowNumber + 1, columnNumber + 1)
-                    ).getText().then(actual => {
-                assertNearlyEqual(Number(actual), value);
-            });
-        }));
+        it('resists script injection attempts', async function() {
+            this.timeout(20000);
+            await driver.get('http://localhost:50054');
+            await goToCell(driver, 'INPUT', 1, 1);
+            const box = await driver.switchTo().activeElement();
+            const text = "<script>alert('bad!')</script>";
+            await box.sendKeys(text, Key.TAB);
+            await driver.wait(until.elementTextContains(box, text));
+        });
+
+        it('is readable from the calculation engine', async function() {
+            this.timeout(20000);
+            await driver.get('http://localhost:50054');
+            await driver.wait(until.elementLocated(cellInTable('INPUT', 1, 1)));
+            await driver.wait(driver => tryToClearGrid(driver), 3000);
+            const u235toU238 = 137.818;
+            const testData = [
+                ['25.2', '0.03', '0.0513', '0.0001'],
+                ['25.4', '0.02', '0.0512', '0.0002'],
+                ['27.1', '0.01', '0.05135', '0.00005']
+            ];
+            await inputTestData(driver, testData);
+            const agesChoiceId = 'ui-id-9';
+            await choosePlotDevice(driver, agesChoiceId);
+            await clickButton(driver, 'run');
+            const expectedResults = [
+                [251.1, 0.51, 250.86, 0.29, 253.3, 4.48, 250.88, 0.29],
+                [248.92, 0.88, 248.93, 0.19, 248.81, 8.99, 248.93, 0.19],
+                [235.59, 0.22, 233.591, 0.085, 255.54, 2.24, 233.619, 0.085]
+            ];
+            await chainWithIndex(expectedResults, (row, rowNumber) =>
+                chainWithIndex(row, (value, columnNumber) => {
+                return driver.findElement(
+                        cellInTable('OUTPUT', rowNumber + 1, columnNumber + 1)
+                        ).getText().then(actual => {
+                    assertNearlyEqual(Number(actual), value);
+                });
+            }));
+        });
+    });
+
+    describe('language support', function() {
+        it('displays the correct language', async function() {
+            this.timeout(20000);
+            await testTranslation(driver, '中文', '帮助', '测量值。',
+                '传递外部误差？',
+                '选择以下四个选项之一',
+                '在线使用', '是一个免费的开源软件');
+            await testTranslation(driver, 'English', 'Help', 'ratios.',
+                'Propagate external uncertainties?',
+                'Choose one of the following four options:',
+                'Online', 'free and open-source');
+        });
     });
 });
+
+async function testTranslation(driver, language, help, ratios, propagate, inputErrorHelp, online, intro) {
+    await driver.get('http://localhost:50054');
+    await chooseLanguage(driver, language);
+    // test dictionary_id.json
+    await assertTextContains(driver, 'help', help);
+    await clickButton(driver, 'help');
+    await assertTextContains(driver, 'UPb_86', ratios);
+    // test dictionary_class.json
+    await clickButton(driver, 'options');
+    await assertTextContains(driver, 'help_exterr_UPb', propagate);
+    // test contextual_help.json
+    await clickButton(driver, 'help_ierr');
+    await assertTextContains(driver, 'helpmenu', inputErrorHelp);
+    // test home_id.json
+    await clickButton(driver, 'home');
+    await assertTextContains(driver, 'online_tab', online);
+    await assertTextContains(driver, 'intro', intro);
+}
+
+async function assertTextContains(driver, id, text) {
+    const element = await driver.wait(until.elementLocated(By.id(id)));
+    await driver.wait(until.elementTextContains(element, text));
+}
 
 function chainWithIndex(arr, callback, first=0, end=arr.length) {
     if (first < end) {
@@ -89,15 +124,25 @@ function assertNearlyEqual(a, b) {
 async function choosePlotDevice(driver, choiceId) {
     // for some reason just clicking doesn't work
     const plotDeviceButton = await driver.findElement(By.id('plotdevice-button'));
-    await driver.actions()
-        .move({ origin: plotDeviceButton })
-        .press()
-        .release()
-        .perform();
+    await performClick(driver, plotDeviceButton);
     const choice = await driver.findElement(By.id(choiceId));
     await driver.wait(until.elementIsVisible(choice));
-    await driver.actions()
-        .move({ origin: choice })
+    await performClick(driver, choice);
+}
+
+async function chooseLanguage(driver, languageText) {
+    const menuButton = await driver.wait(until.elementLocated(By.id('language-button')));
+    await performClick(driver, menuButton);
+    const arbitraryLanguageChoice = await driver.wait(until.elementLocated(By.css('#language-menu li')));
+    await driver.wait(until.elementIsVisible(arbitraryLanguageChoice));
+    const choice = await findMenuItem(driver, languageText);
+    await performClick(driver, choice);
+}
+
+// to be used when normal clicks mysteriously don't work
+async function performClick(driver, element) {
+    driver.actions()
+        .move({origin: element})
         .press()
         .release()
         .perform();
@@ -137,8 +182,22 @@ async function inputTestData(driver, testData) {
     await box2.sendKeys(Key.CONTROL + 'v');
 }
 
+async function findMenuItem(driver, text) {
+    const uiMenuItemLocator = By.className('ui-menu-item-wrapper');
+    driver.wait(until.elementLocated(uiMenuItemLocator));
+    const menuItems = await driver.findElements(uiMenuItemLocator);
+    for (const index in menuItems) {
+        const item = menuItems[index];
+        const itemText = await item.getText();
+        if (itemText === text) {
+            return item;
+        }
+    }
+    assert(false, "No ui menu item found with text '" + text + "'");
+}
+
 async function clickButton(driver, id) {
-    await driver.findElement(By.id(id)).click();
+    await driver.wait(until.elementLocated(By.id(id))).click();
 }
 
 async function goToCell(driver, tableId, row, column) {
