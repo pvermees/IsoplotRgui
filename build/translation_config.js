26dab291d5767da201a1070a1337749e1df66be2
Tim Band <t.band>
2020-05-21 11:50:06 +0100

    home_id translations split

---
+++ build/translation_config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    filenames: [
+        'js/dictionary_id.json',
+        'js/dictionary_class.json',
+        'js/contextual_help.json',
+        'home/home_id.json'
+    ],
+    languageRenames: {
+        'zh-CN': 'cn'
+    }
+};
