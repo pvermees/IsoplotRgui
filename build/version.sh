fe3afa4820040e6bbb5065096daa554bf5d54ff1
Pieter Vermeesch <p.vermeesch@ucl.ac.uk>
2020-09-03 19:40:17 +0100

    changed pvermees/isoplotrgui to pvermees/isoplotr in README file

--- build/version.sh
+++ build/version.sh
@@ -3,14 +3,14 @@
 cd ~/Documents/Programming/R/IsoplotR/
 APP="$(/usr/bin/git rev-list master --count)"
 DIF="$(git rev-list --right-only --count origin/master...origin/beta)"
-APPVERSION=$(($APP+$DIF+1)) # comment out if deploying beta branch
-#APPVERSION=$APP # comment out if deploying master branch
+#APPVERSION=$(($APP+$DIF+1)) # comment out if deploying beta branch
+APPVERSION=$APP # comment out if deploying master branch
 
 cd ~/Documents/Programming/R/IsoplotRgui/
 GUI="$(/usr/bin/git rev-list master --count)"
 DIF="$(git rev-list --right-only --count origin/master...origin/beta)"
-GUIVERSION=$(($GUI+$DIF+1)) # comment out if deploying beta branch
-#GUIVERSION=$GUI # comment out if deploying master branch
+#GUIVERSION=$(($GUI+$DIF+1)) # comment out if deploying beta branch
+GUIVERSION=$GUI # comment out if deploying master branch
 
 s1="/"
 
